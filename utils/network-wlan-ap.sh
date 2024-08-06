77320e2fa2fa0afd4b1f9c462bda8ef0eb6b9ff1
Matt Z <matt@greendormer.com>
2024-08-05 21:17:14 -0700

    added hotspot code

--- utils/network-wlan-ap.sh
+++ utils/network-wlan-ap.sh
@@ -5,12 +5,17 @@
 #               Allows other devices in remote deployments without a network to connect
 # https://www.raspberrypi.com/documentation/computers/configuration.html#host-a-wireless-network-on-your-raspberry-pi
 # See examples: https://networkmanager.dev/docs/api/latest/nmcli-examples.html
+    # More examples: https://www.golinuxcloud.com/nmcli-command-examples-cheatsheet-centos-rhel/ 
 
 #enable hotspot
 sudo nmcli device wifi hotspot ssid <example-network-name> password <example-password>
 
 #disable hotspot
 #sudo nmcli device disconnect wlan0
+
+#to get hotspot to run on restart...
+# sudo -H nano /etc/NetworkManager/system-connections/Hotspot.nmconnection
+# look for `autoconnect=false` and change to true
 
 #list available wifi aps
 #nmcli device wifi list
@@ -23,3 +28,6 @@ sudo nmcli device wifi hotspot ssid <example-network-name> password <example-pas
 
 #convenient field values retrieval for scripting
 #nmcli -g ip4.address connection show my-con-eth0
+
+#create ethernet static IP address
+#sudo nmcli con add con-name eth-static type ethernet ifname eth0 ipv4.method manual ipv4.address 192.168.1.15/24 ipv4.gateway 192.168.1.1
