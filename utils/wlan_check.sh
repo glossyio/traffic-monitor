b8f064039a2bf55d9443e13e310db5a5ef76b6f8
Matt Z <matt@greendormer.com>
2024-10-30 12:52:02 -0700

    created network check script and cron append

---
+++ utils/wlan_check.sh
@@ -0,0 +1,55 @@
+#!/bin/bash
+# Script will check if wlan0 is operating nominally and if not, reset the device
+
+ERROR=false
+LOGFILE=/var/log/wlan_check.log
+DOMAINADDR='duckduckgo.com'
+GWADDR=$(ip route show | grep "^default" | cut -d\  -f3)
+PUBNSADDRS=("8.8.8.8","1.1.1.1","9.9.9.9")
+NETRESTARTCMD="sudo systemctl restart networking"
+
+
+_logoutput(){
+  echo "$(date +%D_%T): $*" >> $LOGFILE 2>&1
+}
+_logonerror(){
+	CMD="$*"
+	OUTPUT=$($CMD 2>&1)
+	ES=$?
+	if ((ES))
+	then
+		echo "$(date +%D_%T): ERROR: $OUTPUT" >> $LOGFILE
+		ERROR=true
+	fi
+	
+}
+_logoutput "==============================================================================="
+_logoutput "Starting wlan_check"
+
+_logoutput "ThingsBoard: $DOMAINADDR"
+_logonerror "host $DOMAINADDR"
+_logonerror "ping -c2 $DOMAINADDR"
+
+_logoutput "Gateway: $GWADDR"
+_logonerror "ping -c2 $GWADDR"
+
+for ns in $(grep "^nameserver" /etc/resolv.conf|tr -s " " | cut -d\  -f2 | tr "\n" " ")
+do
+	_logoutput "Nameserver: $ns"
+	_logonerror "ping -c2 $ns"
+done
+
+for pubns in ${PBNSADDRS[@]}
+do
+	_logoutput "Nameserver: $pubns"
+	_logonerror "ping -c2 $pubns"
+done
+
+if [[ $ERROR != "false" ]]
+then
+	_logoutput "$($NETRESTARTCMD)"
+	exit 1
+else
+	_logoutput "No Network Issues Detected"
+	exit 0
+fi
