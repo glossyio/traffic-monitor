dd99b28af10adaec224db0e2ba586f5fafb2598e
Mark Linehan <mhl@mlinehan.us>
2024-11-28 10:35:10 -0800

    correct typo in msg to see log output

--- utils/wlan_check.sh
+++ utils/wlan_check.sh
@@ -3,7 +3,7 @@
 
 # -q option runs this script without output unless there's an error
 
-# use "journald -r -t wlan_check_log" to see output
+# use "journalctl -r -t wlan_check_log" to see output
 QUIET="no"
 while getopts "q" opt; do
 	case $opt in
