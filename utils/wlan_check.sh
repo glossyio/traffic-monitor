d58ffa7bb1414198d5fb6bffe1ea1e9f14fcbbd0
Ernst Kloppenburg <ernst.kloppenburg@gmx.de>
2025-02-01 20:53:51 +0100

    build on connectivity check feature of networkmanager

--- utils/wlan_check.sh
+++ utils/wlan_check.sh
@@ -17,9 +17,6 @@ while getopts "q" opt; do
 done
 
 ERROR=false
-DOMAINADDR='duckduckgo.com'
-GWADDR=$(ip route show | grep "^default" | cut -d\  -f3)
-PUBNSADDRS=("8.8.8.8" "1.1.1.1" "9.9.9.9")
 NETRESTARTCMD="systemctl restart NetworkManager"
 
 
@@ -32,59 +29,15 @@ _logoutput(){
   		_logallways "$*"
 	fi
 }
-_logonerror(){
-	CMD="$*"
-	OUTPUT=$($CMD 2>&1)
-	ES=$?
-	if ((ES))
-	then
-		_logallways "ERROR: $OUTPUT"
-		ERROR=true
-	fi
-}
-# try a command N times before declaring that it has failed
-_repeat_command() {
-	local CMD="$1"
-  	local times="$2"
-	for ((i = 0; i < times; i++)); do
-		OUTPUT=$($CMD 2>&1)
-		# Check if the command failed (non-zero exit status)
-		if [ $? -ne 0 ]
-		then
-			_logallways "ERROR on iteration $((i + 1)): $OUTPUT"
-		else
-			return 0
-		fi
-	done
-	ERROR=true
-}
-
 
 _logoutput "Starting wlan_check"
 
-_logoutput "External DNS name: $DOMAINADDR"
-_logonerror "host $DOMAINADDR"
-_repeat_command "ping -c1 $DOMAINADDR" 3
+CONN_STATUS=$(nmcli networking connectivity)
 
-_logoutput "Gateway: $GWADDR"
-_repeat_command "ping -c1 $GWADDR" 3
-
-for ns in $(grep "^nameserver" /etc/resolv.conf|tr -s " " | cut -d\  -f2 | tr "\n" " ")
-do
-	_logoutput "Nameserver: $ns"
-	_repeat_command "ping -c1 $ns" 3
-done
-
-for pubns in ${PUBNSADDRS[@]}
-do
-	_logoutput "Nameserver: $pubns"
-	_repeat_command "ping -c1 $pubns" 3
-done
-
-if [[ $ERROR != "false" ]]
+if [[ $CONN_STATUS != "full" ]]
 then
 	#before network reset, log link status
-	_logallways "***** Repeated connectivity errors occurred, dumping link status and restarting network interface *****"
+	_logallways "***** Connectivity lost, dumping link status and restarting network interfaces *****"
 	_IP=$(hostname -I) || true
 	if [ "$_IP" ]; then
 		_logallways "IP addresses: $_IP"
